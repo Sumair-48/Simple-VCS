d666c4fec9975e6b6780d668421f16536afdbdc0
Sumair Uddin Siddiqui <sumair.siddiqui.47@gmail.com>
2025-06-06 19:56:36 +0500

    rm src

--- Model/src_file.py
+++
@@ -1,7 +0,0 @@
-import os
-
-VCS_DIR = ".svcs"
-OBJECTS_DIR = os.path.join(VCS_DIR, "objects")
-INDEX_FILE = os.path.join(VCS_DIR, "index")
-LOG_FILE = os.path.join(VCS_DIR, "log")
-HEAD_FILE = os.path.join(VCS_DIR, "HEAD")
